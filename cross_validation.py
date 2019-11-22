748765bd782478d0f02c57b5acc5f88b3d5ac2b5
Theed <hello@theed.ml>
2019-11-21 22:55:20 -0800

    Include the cross_validate  function

--- cross_validation.py
+++ cross_validation.py
@@ -60,3 +60,39 @@ def cross_validate_clf_optimize(design_matrix, labels, classifier, cv_folds):
         pos_idx = list(classifier.best_estimator_.classes_).index(1)
         pred[te] = (classifier.predict_proba(design_matrix[te,:]))[:, pos_idx]
     return pred
+
+def cross_validate(design_matrix, labels, regressor, cv_folds):
+    """ Perform a cross-validation and returns the predictions. 
+    Use a scaler to scale the features to mean 0, standard deviation 1.
+    
+    Parameters:
+    -----------
+    design_matrix: (n_samples, n_features) np.array
+        Design matrix for the experiment.
+    labels: (n_samples, ) np.array
+        Vector of labels.
+    classifier:  Regressor instance; must have the following methods:
+        - fit(X, y) to train the regressor on the data X, y
+        - predict_proba(X) to apply the trained regressor to the data X and return predicted values
+    cv_folds: sklearn cross-validation object
+        Cross-validation iterator.
+        
+    Return:
+    -------
+    pred: (n_samples, ) np.array
+        Vectors of predictions (same order as labels).
+    """
+    
+    import numpy as np
+    from sklearn.preprocessing import StandardScaler
+    
+    n_classes = np.unique(labels).size
+    pred = np.zeros((labels.shape[0], n_classes))
+    for tr, te in cv_folds:
+        scaler = StandardScaler()
+        Xtr = scaler.fit_transform(design_matrix[tr,:])
+        ytr =  labels[tr]
+        Xte = scaler.transform(design_matrix[te,:])
+        regressor.fit(Xtr, ytr)
+        pred[te, :] = regressor.predict_proba(Xte)
+    return pred
